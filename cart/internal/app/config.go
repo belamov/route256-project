0b8a895b7053f136b58e2b4a512cf8cfb83cb132
belamov <business@belamov.com>
2023-10-04 17:07:54 +0500

    [GOHW-3] ci

--- cart/internal/app/config.go
+++ cart/internal/app/config.go
@@ -8,17 +8,17 @@ import (
 )
 
 type Config struct {
-	HttpServerAddress        string `default:"0.0.0.0:8080"`
-	ProductHttpServiceUrl    string `default:"http://route256.pavl.uk:8080/get_product"`
-	ProductGrpcServiceUrl    string `default:"route256.pavl.uk:8082"`
-	LomsHttpServiceUrl       string `default:"http://localhost:8080"`
-	GrpcServerAddress        string `default:"localhost:8083"`
-	GrpcGatewayServerAddress string `default:"0.0.0.0:8084"`
-	LomsGrpcServiceUrl       string `default:"localhost:8083"`
-	DbUser                   string `default:"postgres"`
-	DbPassword               string `default:"password"`
-	DbHost                   string `default:"db:5432"`
-	DbName                   string `default:"cart"`
+	HttpServerAddress        string `default:"0.0.0.0:8080" split_words:"true"`
+	ProductHttpServiceUrl    string `default:"http://route256.pavl.uk:8080/get_product" split_words:"true"`
+	ProductGrpcServiceUrl    string `default:"route256.pavl.uk:8082" split_words:"true"`
+	LomsHttpServiceUrl       string `default:"http://localhost:8080" split_words:"true"`
+	GrpcServerAddress        string `default:"localhost:8083" split_words:"true"`
+	GrpcGatewayServerAddress string `default:"0.0.0.0:8084" split_words:"true"`
+	LomsGrpcServiceUrl       string `default:"localhost:8083" split_words:"true"`
+	DbUser                   string `default:"postgres" split_words:"true"`
+	DbPassword               string `default:"password" split_words:"true"`
+	DbHost                   string `default:"db:5432" split_words:"true"`
+	DbName                   string `default:"cart" split_words:"true"`
 }
 
 func BuildConfig() *Config {
